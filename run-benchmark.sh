33332a12af63a1add702854b157d273a0dffdf5f
Mykola Pokhylets <mykola.pokhylets@gmail.com>
2024-01-16 17:24:54 +0100

    Added codesigning and switching between debug and release versions

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,5 +1,14 @@
 set -ue
 
+MY_DIR="$(readlink -f "$(dirname $0)")"
+
+DEBUG_SUFFIX=""
+if [ "${1:-}" == "--debug" ]
+then
+    DEBUG_SUFFIX="-debug"
+    shift 1
+fi
+
 if [ -z "${DEVELOPER_DIR+x}" ]
 then
     DEVELOPER_DIR=$(xcode-select -p)
@@ -7,13 +16,28 @@ fi
 
 if [ -z "${RELEASE_BUILD_DIR+x}" ]
 then
-    RELEASE_BUILD_DIR="$(readlink -f "$(dirname $0)/../build/Ninja-ReleaseAssert/swift-macosx-arm64")"
+    RELEASE_BUILD_DIR="$MY_DIR/../build/Ninja-ReleaseAssert/swift-macosx-arm64"
 fi
 
-OUTPUT_NAME=./deinit-benchmark.out
+if [ -z "${DEBUG_BUILD_DIR+x}" ]
+then
+    DEBUG_BUILD_DIR="$MY_DIR/../build/Ninja+cmark-DebugAssert+llvm-RelWithDebInfoAssert+swift-DebugAssert+stdlib-DebugAssert/swift-macosx-arm64"
+fi
 
-SDKROOT=/Applications/Xcode-15.0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
-    $RELEASE_BUILD_DIR/bin/swiftc \
+if [ -z "${BUILD_DIR+x}" ]
+then
+    if [ -z "$DEBUG_SUFFIX" ]
+    then
+        BUILD_DIR="$RELEASE_BUILD_DIR"
+    else
+        BUILD_DIR="$DEBUG_BUILD_DIR"
+    fi
+fi
+
+OUTPUT_NAME="$MY_DIR/deinit-benchmark$DEBUG_SUFFIX.out"
+
+SDKROOT="$DEVELOPER_DIR/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk" \
+    $BUILD_DIR/bin/swiftc \
         -target arm64-apple-macosx10.13  \
         -F "$DEVELOPER_DIR/Platforms/MacOSX.platform/Developer/Library/Frameworks" \
         -toolchain-stdlib-rpath \
@@ -33,12 +57,13 @@ SDKROOT=/Applications/Xcode-15.0.app/Contents/Developer/Platforms/MacOSX.platfor
         -Xfrontend -define-availability -Xfrontend 'SwiftStdlib 5.7:macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0' \
         -Xfrontend -define-availability -Xfrontend 'SwiftStdlib 5.8:macOS 13.3, iOS 16.4, watchOS 9.4, tvOS 16.4' \
         -Xfrontend -define-availability -Xfrontend 'SwiftStdlib 5.9:macOS 9999, iOS 9999, watchOS 9999, tvOS 9999' \
-        -F "$RELEASE_BUILD_DIR/lib" \
-        -Xlinker -rpath -Xlinker "$RELEASE_BUILD_DIR/lib" \
-        -module-cache-path $RELEASE_BUILD_DIR/swift-test-results/arm64-apple-macosx10.13/clang-module-cache \
+        -F "$BUILD_DIR/lib" \
+        -Xlinker -rpath -Xlinker "$BUILD_DIR/lib" \
+        -module-cache-path $BUILD_DIR/swift-test-results/arm64-apple-macosx10.13/clang-module-cache \
         deinit-benchmark.swift \
         -Xfrontend -disable-availability-checking \
         -g -Onone \
         -o "$OUTPUT_NAME" \
         -module-name main && \
-/usr/bin/env DYLD_LIBRARY_PATH="$RELEASE_BUILD_DIR/lib/swift/macosx" "$OUTPUT_NAME" "$@"
+codesign -s - -v -f --entitlements "$MY_DIR/entitlements.plist" "$OUTPUT_NAME" && \
+/usr/bin/env DYLD_LIBRARY_PATH="$BUILD_DIR/lib/swift/macosx" "$OUTPUT_NAME" "$@"
